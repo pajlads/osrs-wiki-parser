1a751e0a4117d44ef2a3178a263625ee206b0b28
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-10-20 17:48:53 +0200

    add `quests-by-optimal-order` command

--- main.py
+++ main.py
@@ -3,21 +3,39 @@
 # the idea is for this to be expanded to other quest orders
 
 import datetime
+from enum import Enum
+from operator import attrgetter
 import re
 import sys
 from dataclasses import dataclass
-from functools import cmp_to_key
-from operator import attrgetter
 from typing import Optional
 
 from bs4 import BeautifulSoup, NavigableString, Tag
 
 UNIMPLEMENTED_QUESTS = ("The Frozen Door", "Into the Tombs")
 
+QUEST_HELPER_CUSTOM_ORDER = [
+    "Barbarian Training",
+    "Bear Your Soul",
+    "Enchanted Key",
+    "Family Pest",
+    "Mage Arena I",
+    "Mage Arena II",
+    "All Hard Achievement Diaries",
+    "All Elite Achievement Diaries",
+]
+
 
 def clean_quest_name(quest_name: str) -> str:
     return (
         quest_name.upper()
+        .replace(
+            "KOUREND & KEBOS", "KOUREND"
+        )  # for achievement diaries, should be removed
+        .replace(
+            "WESTERN PROVINCES", "WESTERN"
+        )  # for achievement diaries, should be removed
+        .replace(" DIARY", "")  # for achievement diaries, should be removed
         .replace(" ", "_")
         .replace("'", "")
         .replace("&", "")
@@ -29,14 +47,22 @@ def clean_quest_name(quest_name: str) -> str:
 
 def find_wiki_table(name: str):
     def is_wiki_table(tag: Tag) -> bool:
+        MIN_HEADERS = ["quest/activity", "release date"]
         # print(f"Tag: {tag}")
         if tag.name != "table":
             return False
         if "wikitable" not in tag.attrs["class"]:
             return False
         headers = [tag.text.strip().lower() for tag in tag.find_all("th")]
-        if "name" not in headers or "release date" not in headers:
+        matches = 0
+        for header in MIN_HEADERS:
+            if header in headers:
+                matches += 1
+
+        if matches == 0:
             return False
+        # if "name" not in headers or "release date" not in headers:
+        #     return False
         section = tag.find_previous("span", attrs={"class": "mw-headline"})
         if section is None:
             return False
@@ -45,8 +71,22 @@ def find_wiki_table(name: str):
     return is_wiki_table
 
 
+class QuestType(Enum):
+    FREE_TO_PLAY_QUEST = 1
+    MEMBERS_QUEST = 2
+    MINI_QUEST = 3
+    ACHIEVEMENT_DIARY = 4
+
+    # e.g. Stronghold of Security
+    CUSTOM_QUEST = 5
+
+    BALLOON_UNLOCK = 5
+
+
 @dataclass
 class Quest:
+    quest_type: QuestType
+
     # e.g. 100
     number: Optional[int]
 
@@ -62,19 +102,51 @@ class Quest:
     # e.g. "Very Short"
     length: str
 
-    quest_points: Optional[int]
+    quest_points: int
 
     # e.g. "Demon Slayer"
     series: Optional[str]
 
-    release_date_str: str
     release_date: datetime.datetime
+
+    optimal_order: int
+
+    optimal_ironman_order: int
+
+    qh_order: int
 
     def quest_helper_enum_values(self) -> list[str]:
         if self.name in UNIMPLEMENTED_QUESTS:
             return [
                 f"//QuestHelperQuest.{clean_quest_name(self.name)}, - Placeholder for future addition."
             ]
+
+        if self.quest_type == QuestType.BALLOON_UNLOCK:
+            return [
+                f"QuestHelperQuest.{clean_quest_name(self.name.replace("system to ", ""))}",
+            ]
+
+        if self.quest_type == QuestType.ACHIEVEMENT_DIARY:
+            # TODO: Fix this to just align with osrs wiki naming?
+
+            if "Easy" in self.name:
+                return [
+                    f"QuestHelperQuest.{clean_quest_name(self.name.replace("Easy ", ""))}_EASY",
+                ]
+            elif "Medium" in self.name:
+                return [
+                    f"QuestHelperQuest.{clean_quest_name(self.name.replace("Medium ", ""))}_MEDIUM",
+                ]
+            elif "Hard" in self.name:
+                return [
+                    f"QuestHelperQuest.{clean_quest_name(self.name.replace("Hard ", ""))}_HARD",
+                ]
+            elif "Elite" in self.name:
+                return [
+                    f"QuestHelperQuest.{clean_quest_name(self.name.replace("Elite ", ""))}_ELITE",
+                ]
+            else:
+                assert f"unhandled achievement diary difficulty for {self.name}"
 
         if self.name == "Shield of Arrav":
             return [
@@ -172,7 +244,11 @@ class Quest:
         return [f"QuestHelperQuest.{clean_quest_name(self.name)}"]
 
 
-def get_quests(quest_table: Tag | NavigableString | None) -> list[Quest]:
+def get_quests(
+    quest_table: Tag | NavigableString | None,
+    quest_type: QuestType,
+    optimal_quest_order: list[str],
+) -> list[Quest]:
     assert isinstance(quest_table, Tag)
 
     quests: list[Quest] = []
@@ -192,7 +268,14 @@ def get_quests(quest_table: Tag | NavigableString | None) -> list[Quest]:
                 r" +", " ", data[i].text.strip().replace("\n", "")
             )
 
-        series = quest_data["series"] or "N/A"
+        quest_name = quest_data["name"].strip()
+
+        series: Optional[str] = None
+        if "series" in quest_data:
+            series = quest_data["series"]
+            if series == "N/A":
+                series = None
+
         number_str = quest_data.get("#", None)
         number: Optional[int] = None
         subnumber: Optional[int] = None
@@ -206,23 +289,239 @@ def get_quests(quest_table: Tag | NavigableString | None) -> list[Quest]:
 
         quest_points = 0
         if "" in quest_data:
+            # mini quests don't have quest points
             quest_points = int(quest_data[""])
 
-        release_date_str = quest_data["release date"]
-        release_date = datetime.datetime.strptime(release_date_str, "%d %B %Y")
+        release_date = datetime.datetime.strptime(
+            quest_data["release date"], "%d %B %Y"
+        )
+
+        try:
+            optimal_order = optimal_quest_order.index(quest_name)
+        except ValueError:
+            optimal_order = -1
+
+        optimal_ironman_order = -1
+
+        try:
+            qh_order = QUEST_HELPER_CUSTOM_ORDER.index(quest_name)
+        except ValueError:
+            qh_order = -1
 
         quests.append(
             Quest(
+                quest_type,
                 number,
                 subnumber,
-                quest_data["name"],
+                quest_name,
                 quest_data["difficulty"],
                 quest_data["length"],
                 quest_points,
-                None if series == "N/A" else series,
-                release_date_str,
+                series,
                 release_date,
+                optimal_order,
+                optimal_ironman_order,
+                qh_order,
             )
+        )
+
+    return quests
+
+
+def custom_quest(
+    name: str, release_date: datetime.datetime, optimal_quest_order: list[str]
+) -> Quest:
+    try:
+        qh_order = QUEST_HELPER_CUSTOM_ORDER.index(name)
+    except ValueError:
+        qh_order = -1
+
+    return Quest(
+        QuestType.CUSTOM_QUEST,
+        None,
+        None,
+        name,
+        "N/A",
+        "N/A",
+        0,
+        None,
+        release_date,
+        optimal_quest_order.index(name),
+        -1,
+        qh_order,
+    )
+
+
+def balloon_unlock(
+    name: str, release_date: datetime.datetime, optimal_quest_order: list[str]
+) -> Quest:
+    try:
+        qh_order = QUEST_HELPER_CUSTOM_ORDER.index(name)
+    except ValueError:
+        qh_order = -1
+
+    return Quest(
+        QuestType.BALLOON_UNLOCK,
+        None,
+        None,
+        name,
+        "N/A",
+        "N/A",
+        0,
+        None,
+        release_date,
+        optimal_quest_order.index(name),
+        -1,
+        qh_order,
+    )
+
+
+def diary(
+    name: str, release_date: datetime.datetime, optimal_quest_order: list[str]
+) -> Quest:
+    try:
+        qh_order = QUEST_HELPER_CUSTOM_ORDER.index(name)
+    except ValueError:
+        if "Hard" in name:
+            qh_order = QUEST_HELPER_CUSTOM_ORDER.index("All Hard Achievement Diaries")
+        elif "Elite" in name:
+            qh_order = QUEST_HELPER_CUSTOM_ORDER.index("All Elite Achievement Diaries")
+        else:
+            qh_order = -1
+
+    if name in optimal_quest_order:
+        optimal_order = optimal_quest_order.index(name)
+    else:
+        if "Easy" in name:
+            optimal_order = optimal_quest_order.index("All Easy Achievement Diaries")
+        else:
+            optimal_order = -1
+    return Quest(
+        QuestType.ACHIEVEMENT_DIARY,
+        None,
+        None,
+        name,
+        "N/A",
+        "N/A",
+        0,
+        None,
+        release_date,
+        optimal_order,
+        -1,
+        qh_order,
+    )
+
+
+def load_quest_list(
+    optimal_quest_order: list[str],
+) -> list[Quest]:
+    with open("data/quest-list.html", "r") as fh:
+        html = "".join(fh.readlines())
+    data = BeautifulSoup(html, "html.parser")
+
+    DIARY_DIFFICULTIES = ["Easy", "Medium", "Hard", "Elite"]
+    DIARIES = [
+        ("Ardougne", datetime.datetime(2015, 3, 5)),
+        ("Desert", datetime.datetime(2015, 3, 5)),
+        ("Falador", datetime.datetime(2015, 3, 5)),
+        ("Fremennik", datetime.datetime(2015, 3, 5)),
+        ("Kandarin", datetime.datetime(2015, 3, 5)),
+        ("Karamja", datetime.datetime(2007, 5, 8)),  # this is not entirely correct
+        ("Kourend & Kebos", datetime.datetime(2019, 1, 10)),
+        ("Lumbridge", datetime.datetime(2015, 3, 5)),
+        ("Morytania", datetime.datetime(2015, 3, 5)),
+        ("Varrock", datetime.datetime(2015, 3, 5)),
+        ("Western Provinces", datetime.datetime(2015, 3, 5)),
+        ("Wilderness", datetime.datetime(2015, 3, 5)),
+    ]
+
+    custom_quests = [
+        custom_quest(
+            "Stronghold of Security", datetime.datetime(2006, 7, 4), optimal_quest_order
+        ),
+        custom_quest(
+            "Knight Waves Training Grounds",
+            datetime.datetime(2007, 7, 24),
+            optimal_quest_order,
+        ),
+        balloon_unlock(
+            "Balloon transport system to Crafting Guild",
+            datetime.datetime(2006, 11, 6),
+            optimal_quest_order,
+        ),
+        balloon_unlock(
+            "Balloon transport system to Varrock",
+            datetime.datetime(2006, 11, 6),
+            optimal_quest_order,
+        ),
+        balloon_unlock(
+            "Balloon transport system to Castle Wars",
+            datetime.datetime(2006, 11, 6),
+            optimal_quest_order,
+        ),
+        balloon_unlock(
+            "Balloon transport system to Grand Tree",
+            datetime.datetime(2006, 11, 6),
+            optimal_quest_order,
+        ),
+    ]
+    for difficulty in DIARY_DIFFICULTIES:
+        for region, release_date in DIARIES:
+            custom_quests.append(
+                diary(f"{difficulty} {region} Diary", release_date, optimal_quest_order)
+            )
+
+    f2p_quests = get_quests(
+        data.find(find_wiki_table("Free-to-play quests")),
+        QuestType.FREE_TO_PLAY_QUEST,
+        optimal_quest_order,
+    )
+    members_quests = get_quests(
+        data.find(find_wiki_table("Members' Quests")),
+        QuestType.MEMBERS_QUEST,
+        optimal_quest_order,
+    )
+    mini_quests = get_quests(
+        data.find(find_wiki_table("Miniquests")),
+        QuestType.MINI_QUEST,
+        optimal_quest_order,
+    )
+
+    return custom_quests + f2p_quests + members_quests + mini_quests
+
+
+def load_optimal_quest_order() -> list[str]:
+    with open("data/optimal-quest-guide.html", "r") as fh:
+        html = "".join(fh.readlines())
+    data = BeautifulSoup(html, "html.parser")
+
+    quest_table = data.find(find_wiki_table("Quests"))
+    assert isinstance(quest_table, Tag)
+
+    quests: list[str] = []
+
+    headers = [tag.text.strip().lower() for tag in quest_table.find_all("th")]
+    for row in quest_table.find_all("tr"):
+        assert isinstance(row, Tag)
+
+        data = row.find_all("td")
+        if not data:
+            # skip header / empty rows
+            continue
+
+        quest_data = {}
+        for i in range(0, len(data)):
+            quest_data[headers[i]] = re.sub(
+                r" +", " ", data[i].text.strip().replace("\n", "")
+            )
+
+        assert "quest/activity" in quest_data
+
+        quests.append(
+            quest_data["quest/activity"]
+            .replace("Unlock:", "")
+            .replace("(miniquest)", "")
+            .strip()
         )
 
     return quests
@@ -237,20 +536,27 @@ def sort_by_release_date(quest: Quest):
     )
 
 
-def print_quest_order_by_release_date(
-    f2pQuests: list[Quest], membersQuests: list[Quest], miniQuests: list[Quest]
-) -> None:
+def print_quest_order_by_release_date(quests: list[Quest]) -> None:
     body = ""
+
     body += "\t\t// Quests\n"
-    for quest in sorted(f2pQuests + membersQuests, key=sort_by_release_date):
+    for quest in filter(
+        lambda q: q.quest_type
+        in (QuestType.FREE_TO_PLAY_QUEST, QuestType.MEMBERS_QUEST),
+        sorted(quests, key=sort_by_release_date),
+    ):
         quest_enums = quest.quest_helper_enum_values()
         for s in quest_enums:
             if s.strip().startswith("//"):
                 body += f"\t\t{s}\n"
             else:
                 body += f"\t\t{s},\n"
+
     body += "\t\t// Miniquests\n"
-    for quest in sorted(miniQuests, key=sort_by_release_date):
+    for quest in filter(
+        lambda q: q.quest_type == QuestType.MINI_QUEST,
+        sorted(quests, key=sort_by_release_date),
+    ):
         quest_enums = quest.quest_helper_enum_values()
         for s in quest_enums:
             if s.strip().startswith("//"):
@@ -261,28 +567,52 @@ def print_quest_order_by_release_date(
     print(body.strip().rstrip(","))
 
 
-def load_quest_list() -> tuple[list[Quest], list[Quest], list[Quest]]:
-    with open("data/quest-list.html", "r") as fh:
-        html = "".join(fh.readlines())
-    data = BeautifulSoup(html, "html.parser")
+def print_quests_enum_by_optimal_order(quests: list[Quest]) -> None:
+    body = ""
 
-    f2p_quests = get_quests(data.find(find_wiki_table("Free-to-play quests")))
-    members_quests = get_quests(data.find(find_wiki_table("Members' Quests")))
-    mini_quests = get_quests(data.find(find_wiki_table("Miniquests")))
+    for quest in filter(
+        lambda q: q.optimal_order > -1,
+        sorted(quests, key=attrgetter("optimal_order")),
+    ):
+        quest_enums = quest.quest_helper_enum_values()
+        for s in quest_enums:
+            if s.strip().startswith("//"):
+                body += f"\t\t{s}\n"
+            else:
+                body += f"\t\t{s},\n"
 
-    return (f2p_quests, members_quests, mini_quests)
+    unordered_quests = list(
+        filter(
+            lambda q: q.optimal_order == -1,
+            sorted(quests, key=attrgetter("qh_order", "name")),
+        )
+    )
+    if len(unordered_quests) > 0:
+        body += "\t\t// Quests & mini quests that are not part of the OSRS Wiki's Optimal Quest Guide\n"
+
+        for quest in unordered_quests:
+            quest_enums = quest.quest_helper_enum_values()
+            for s in quest_enums:
+                if s.strip().startswith("//"):
+                    continue
+                else:
+                    body += f"\t\t{s},\n"
+
+    print(body.strip().rstrip(","))
 
 
 def main() -> None:
     COMMANDS = [
         "quests-by-release-date",
+        "quests-by-optimal-order",
     ]
 
     command = "quests-by-release-date"
     if len(sys.argv) >= 2:
         command = sys.argv[1]
 
-    f2p_quests, members_quests, mini_quests = load_quest_list()
+    optimal_quest_order = load_optimal_quest_order()
+    quests = load_quest_list(optimal_quest_order)
 
     match command:
         case "quests-by-release-date":
@@ -295,13 +625,28 @@ def main() -> None:
 
             match subcommand:
                 case "enum":
-                    print_quest_order_by_release_date(
-                        f2p_quests, members_quests, mini_quests
-                    )
+                    print_quest_order_by_release_date(quests)
                 case other:
                     print(
                         f"Unknown subcommand '{other}'. Available subcommands: {', '.join(SUBCOMMANDS)}"
                     )
+
+        case "quests-by-optimal-order":
+            SUBCOMMANDS = [
+                "enum",
+            ]
+            subcommand = "enum"
+            if len(sys.argv) >= 3:
+                subcommand = sys.argv[2]
+
+            match subcommand:
+                case "enum":
+                    print_quests_enum_by_optimal_order(quests)
+                case other:
+                    print(
+                        f"Unknown subcommand '{other}'. Available subcommands: {', '.join(SUBCOMMANDS)}"
+                    )
+
         case other:
             print(
                 f"Unknown command '{other}'. Available commands: {', '.join(COMMANDS)}"
